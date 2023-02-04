9f49fd1ecea90511786503d1241ab8b9f7cf8ae9
bahaa <malkebahaa450>
2023-02-04 21:20:16 +0200

    trying to learn how java deal with json files

--- Customer_Sorting/src/Code_For_Customer_Sorting/AcessDatabase.java
+++ Customer_Sorting/src/Code_For_Customer_Sorting/AcessDatabase.java
@@ -2,4 +2,14 @@ package Code_For_Customer_Sorting;
 
 public class AcessDatabase {
 
+	
+	
+	// get the link of the folder of the database
+	// use the read mechanism in Java to get the data from database
+	// database is JSON file how Java deal with JSON files....
+	//
+	
+	
+	
+	
 }
